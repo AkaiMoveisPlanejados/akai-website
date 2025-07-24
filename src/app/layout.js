bbab4e09674a7388ab5af0226b5b6fc4735d86a7
Lucas Costa <lucas.alves.costa96@gmail.com>
2025-07-24 15:47:23 -0300

    Add GTM tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -30,6 +31,16 @@ export default function RootLayout({ children }) {
               gtag('js', new Date());
               gtag('config', 'AW-1005001799');
             `,
+          }}
+        />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5D7VWGG9');`
           }}
         />
       </head>
